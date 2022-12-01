ba5bc6ffacfb9c6a59bec84d3180e71e4d4fe99b
bzab076 <bzab076@gmail.com>
2022-12-01 06:35:39 +0100

    day 1

--- src/main/scala/CommonPuzzle.scala
+++ src/main/scala/CommonPuzzle.scala
@@ -7,11 +7,30 @@ abstract class CommonPuzzle(day : Int) {
   def partOne : Any
   def partTwo : Any
 
-  def inputLines =
+  def inputLines: List[String] =
     Source.fromResource(resourceName).getLines().toList
 
-  def inputNumbers = inputLines.map(it => it.toInt)
+  def inputNumbers: List[Int] = inputLines.map(it => it.toInt)
 
   def inputString: String = inputLines.head
+
+  def inputAsArrayOfDigits: Array[Array[Int]] = {
+
+    val xSize: Int = inputLines.head.length
+    val ySize: Int = inputLines.length
+    val result = Array.ofDim[Int](ySize, xSize)
+
+    var y = 0
+    for(line <- inputLines) {
+      var x = 0
+      for(char <- line.toList) {
+        result(y)(x) = char - 48
+        x += 1
+      }
+      y += 1
+    }
+
+    result
+  }
 
 }
--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -1,6 +1,26 @@
-object Day01 extends  CommonPuzzle (1){
+import scala.collection.mutable.ArrayBuffer
 
-  override def partOne: Any = "Not yet implemented"
+object Day01 extends  CommonPuzzle (1) {
 
-  override def partTwo: Any = ???
+  val calorieList : List[Int] = getCalories
+
+  override def partOne: Any = calorieList.max
+
+  override def partTwo: Any = calorieList.sorted.reverse.take(3).sum
+
+  def getCalories : List[Int] = {
+    val cals: ArrayBuffer[Int] = ArrayBuffer[Int]()
+    var sum = 0
+    for (line <- inputLines) {
+      if (line.trim.isEmpty) {
+        cals.addOne(sum)
+        sum = 0
+      }
+      else sum += line.toInt
+    }
+    cals.addOne(sum)
+    cals.toList
+  }
+
+
 }

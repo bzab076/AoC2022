a9236470b27dbeffc6570d3f42f465c33eb0e963
bzab076 <bzab076@gmail.com>
2022-12-04 06:38:04 +0100

    refactored day 4

--- src/main/scala/Day04.scala
+++ src/main/scala/Day04.scala
@@ -1,11 +1,10 @@
 object Day04 extends CommonPuzzle (4) {
 
-  override def partOne: Any = inputLines.map { case s"$x1-$y1,$x2-$y2" => (x1.toInt, y1.toInt) -> (x2.toInt, y2.toInt) }
-    .count { case (r1, r2) => rangeContained(Range.inclusive(r1._1, r1._2), Range.inclusive(r2._1, r2._2)) }
+  val input: List[((Int, Int), (Int, Int))] = inputLines.map { case s"$x1-$y1,$x2-$y2" => (x1.toInt, y1.toInt) -> (x2.toInt, y2.toInt) }
 
-  override def partTwo: Any = inputLines.map { case s"$x1-$y1,$x2-$y2" => (x1.toInt, y1.toInt) -> (x2.toInt, y2.toInt) }
-    .count { case (r1, r2) => Range.inclusive(r1._1, r1._2).intersect(Range.inclusive(r2._1, r2._2)).nonEmpty }
+  override def partOne: Any = input.count { case (r1, r2) => rangeContained(Range.inclusive(r1._1, r1._2), Range.inclusive(r2._1, r2._2)) }
 
+  override def partTwo: Any = input.count { case (r1, r2) => Range.inclusive(r1._1, r1._2).intersect(Range.inclusive(r2._1, r2._2)).nonEmpty }
 
   def rangeContained(r1 : Range, r2 : Range) : Boolean = r1 == r1.intersect(r2) || r2 == r1.intersect(r2)
 }

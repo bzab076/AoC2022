c40428b955cf06fc12f7a007ac27b6c62a9c8c60
bzab076 <bzab076@gmail.com>
2022-12-06 20:53:28 +0100

    rewritten day 6 in functional style

--- src/main/scala/Day06.scala
+++ src/main/scala/Day06.scala
@@ -4,6 +4,6 @@ object Day06 extends CommonPuzzle (6) {
 
   override def partTwo: Any = distinctSubstringIndex(14)
 
-  def distinctSubstringIndex(chunkSize : Int) : Int = inputString.toList.sliding(chunkSize).zipWithIndex.toList.filter{ case (l,i) => l.distinct.length == chunkSize}.head._2 + chunkSize
+  def distinctSubstringIndex(chunkSize : Int) : Int = inputString.toList.sliding(chunkSize).zipWithIndex.toList.filter{ case (l,_) => l.distinct.length == chunkSize}.head._2 + chunkSize
 
 }

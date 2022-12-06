b43960a32370a1957ee75405eab49713cdf18cba
bzab076 <bzab076@gmail.com>
2022-12-06 07:21:53 +0100

    rewritten day 6 in functional style

--- src/main/scala/Day06.scala
+++ src/main/scala/Day06.scala
@@ -1,18 +1,9 @@
 object Day06 extends CommonPuzzle (6) {
 
-  override def partOne: Any = getDistinctSubstringIndex(inputString,4)
+  override def partOne: Any = distinctSubstringIndex(4)
 
-  override def partTwo: Any = getDistinctSubstringIndex(inputString,14)
+  override def partTwo: Any = distinctSubstringIndex(14)
 
-  def getDistinctSubstringIndex(str : String, chunkSize : Int) : Int = {
+  def distinctSubstringIndex(chunkSize : Int) : Int = inputString.toList.sliding(chunkSize).zipWithIndex.toList.filter{ case (l,i) => l.distinct.length == chunkSize}.head._2 + chunkSize
 
-    var i = 0
-    while(i < str.length - chunkSize) {
-      val substr = inputString.substring(i, i + chunkSize)
-      if(substr.toCharArray.toList.distinct.size == chunkSize) return i+chunkSize
-      i += 1
-    }
-    0 // default result if no distinct substring is found
-
-  }
 }

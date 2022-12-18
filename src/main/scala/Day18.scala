2dd20b324a8c161e245ce7d9281c69b9a28101c3
bzab076 <bzab076@gmail.com>
2022-12-18 21:29:34 +0100

    day 18

---
+++ src/main/scala/Day18.scala
@@ -0,0 +1,42 @@
+object Day18 extends CommonPuzzle (18) {
+
+  val grid: List[(Int, Int, Int)] = inputLines.map{ case s"$x,$y,$z" => (x.toInt, y.toInt, z.toInt)}
+  val maxX: Int = grid.map { case (x, _, _) => x }.max
+  val minX: Int = grid.map { case (x, _, _) => x }.min
+  val maxY: Int = grid.map { case (_, y, _) => y }.max
+  val minY: Int = grid.map { case (_, y, _) => y }.min
+  val maxZ: Int = grid.map { case (_, _, z) => z }.max
+  val minZ: Int = grid.map { case (_, _, z) => z }.min
+
+  val adjacent = List((1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1))
+
+  override def partOne: Any = surfaceArea(grid)
+
+  override def partTwo: Any = {
+    val airPockets = findAirPockets(grid)
+    surfaceArea(grid.appendedAll(airPockets))
+
+  }
+
+  def surfaceArea(someGrid: List[(Int, Int, Int)]): Int = someGrid.map(c => 6 - adjacentCubes(c, someGrid)).sum
+
+
+  def adjacentCubes(cube: (Int, Int, Int), someGrid: List[(Int, Int, Int)]): Int =
+    adjacent.map{ case(dx,dy,dz) => if(someGrid.contains((cube._1+dx,cube._2+dy,cube._3+dz))) 1 else 0}.sum
+
+  def findAirPockets(someGrid: List[(Int, Int, Int)]): List[(Int, Int, Int)] = {
+    val airPockets0 = (for {
+      x <- minX+1 to maxX-1
+      y <- minY+1 to maxY-1
+      z <- minZ+1 to maxZ-1
+      if !someGrid.contains((x, y, z))
+      if someGrid.filter { case (a, b, c) => a > x && b == y && c == z }.nonEmpty && someGrid.filter { case (a, b, c) => a < x && b == y && c == z }.nonEmpty
+      if someGrid.filter { case (a, b, c) => a == x && b > y && c == z }.nonEmpty && someGrid.filter { case (a, b, c) => a == x && b < y && c == z }.nonEmpty
+      if someGrid.filter { case (a, b, c) => a == x && b == y && c > z }.nonEmpty && someGrid.filter { case (a, b, c) => a == x && b == y && c < z }.nonEmpty
+    } yield (x,y,z)).toList
+
+    // remove outliers
+    airPockets0.filter(cube => adjacentCubes(cube,grid) + adjacentCubes(cube,airPockets0) == 6)
+  }
+
+}
